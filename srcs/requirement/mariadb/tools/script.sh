102e37bb2f2de693bb5bf5c6ea99ac2ff4da9681
hmouis <hmouis@c4r1p2.myguest.virtualbox.org>
2026-09-11 06:11:03 +0100

    ADD:;

--- srcs/requirement/mariadb/tools/script.sh
+++ srcs/requirement/mariadb/tools/script.sh
@@ -14,10 +14,12 @@ mariadb -u root << EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" --socket=/run/mysqld/mysqld.sock shutdown
 fi
+
+
 
 exec mysqld_safe
